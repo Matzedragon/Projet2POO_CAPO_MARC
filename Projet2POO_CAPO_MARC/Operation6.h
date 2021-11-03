59b83f3e2365f174bbc74bf5e4f5bf7bde81b195
Mathieu Capo <mathieu.capo@gmail.com>
2021-11-03 03:46:02 +0100

    ajout méthod effectuerOperation dans operationX

--- Projet2POO_CAPO_MARC/Operation6.h
+++ Projet2POO_CAPO_MARC/Operation6.h
@@ -5,5 +5,6 @@ class Operation6 : public Operation
 public:
 	Operation6(Operation* operationSuivanteTrue, Operation* operationSuivanteFalse);
 	Operation6();
+	bool effecterOperation(Dechet* dechet);
 	~Operation6();
 };
